8a37f53c5630656b17e81297dbc90b9dba23f3be
Nerfo1124 <dfernando1124@hotmail.com>
2017-09-14 14:30:49 -0500

    Proyecto de Estadistica

---
+++ Probabilidad&Estadistica/src/java/co/edu/udistrital/AdminServiceCorteUno.java
@@ -0,0 +1,16 @@
+package co.edu.udistrital;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fernando
+ */
+public class AdminServiceCorteUno {
+    
+    
+}
